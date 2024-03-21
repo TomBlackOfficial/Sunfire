b81e00ac4e8769faa47d838d99263495ce7e9c6f
Tom Black <tomblackbusiness@gmail.com>
2024-03-20 22:06:04 -0700

    Event System

--- Sunfire/src/Sunfire/Application.cpp
+++ Sunfire/src/Sunfire/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Sunfire/Events/ApplicationEvent.h"
+#include "Sunfire/Log.h"
+
 namespace Sunfire {
 
 	Application::Application()
@@ -14,6 +17,16 @@ namespace Sunfire {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			SF_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			SF_TRACE(e);
+		}
+
 		while (true);
 	}
 
--- premake5.lua
+++ premake5.lua
@@ -26,6 +26,7 @@ project "Sunfire"
 
 	includedirs
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
