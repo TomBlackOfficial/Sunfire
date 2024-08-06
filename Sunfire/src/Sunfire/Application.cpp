f8dc7d870963f691bb955878d10c44a95c562cb1
Tom Black <tomblackbusiness@gmail.com>
2024-08-05 17:06:57 -0700

    Window event system

--- Sunfire/src/Sunfire/Application.cpp
+++ Sunfire/src/Sunfire/Application.cpp
@@ -1,21 +1,31 @@
 #include "sfpch.h"
 #include "Application.h"
 
-#include "Sunfire/Events/ApplicationEvent.h"
 #include "Sunfire/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Sunfire {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SF_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,6 +36,12 @@ namespace Sunfire {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Sunfire/src/Sunfire/Application.h
+++ Sunfire/src/Sunfire/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Sunfire/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Sunfire {
@@ -13,7 +15,11 @@ namespace Sunfire {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
